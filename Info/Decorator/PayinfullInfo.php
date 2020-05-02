13ed84bf99c671928ed756ce2a63b88e53beea89
fall1600 <fall1600@gmail.com>
2020-05-03 07:30:48 +0800

    wip

---
+++ Info/Decorator/PayinfullInfo.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace fall1600\Package\Newebpay\Info\Decorator;
+
+use fall1600\Package\Newebpay\Info\Info;
+
+class PayinfullInfo extends Info
+{
+    /** @var Info */
+    protected $info;
+
+    /**
+     * 設定是否啟用信用卡一次付清支付方式
+     * @var bool
+     */
+    protected $isPayinfull;
+
+    public function __construct(Info $info, bool $isPayinfull = true)
+    {
+        $this->info = $info;
+
+        $this->isPayinfull = $isPayinfull;
+    }
+
+    public function getInfo()
+    {
+        return $this->info->getInfo() +
+            [
+                'CREDIT' => $this->isPayinfull? 1: 0,
+            ];
+    }
+}
