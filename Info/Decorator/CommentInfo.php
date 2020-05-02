6efc6afe4e3b3df66692680abb843655878044b6
fall1600 <fall1600@gmail.com>
2020-05-03 07:25:54 +0800

    wip

---
+++ Info/Decorator/CommentInfo.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace fall1600\Package\Newebpay\Info\Decorator;
+
+use fall1600\Package\Newebpay\Info\Info;
+
+class CommentInfo extends Info
+{
+    /** @var Info */
+    protected $info;
+
+    /**
+     * 商店備註
+     *  若提供此參數則會在付款頁面顯示商店資訊
+     * @var string
+     */
+    protected $desc;
+
+    public function __construct(Info $info, string $comment)
+    {
+        $this->info = $info;
+
+        $this->setComment($comment);
+    }
+
+    public function getInfo()
+    {
+        return $this->info->getInfo() +
+            [
+                'OrderComment' => $this->desc,
+            ];
+    }
+
+    protected function setComment(string $comment)
+    {
+        if (mb_strlen($comment) > 300) {
+            throw new \LogicException('comment must less or equal than 300');
+        }
+
+        $this->comment = $comment;
+    }
+}
