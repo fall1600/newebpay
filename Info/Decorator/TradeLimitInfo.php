375928eb422a8bc331edbe17c7a3749e4c6273a9
fall1600 <fall1600@gmail.com>
2020-05-03 08:18:06 +0800

    wip

---
+++ Info/Decorator/TradeLimitInfo.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace fall1600\Package\Newebpay\Info\Decorator;
+
+use fall1600\Package\Newebpay\Info\Info;
+
+class TradeLimitInfo extends Info
+{
+    /** @var Info */
+    protected $info;
+
+    /**
+     * 交易限制秒數, 超過即交易失敗
+     *  0: 不啟用
+     *  1~60: 限制60秒
+     *  上限 900 秒
+     * @var int
+     */
+    protected $limit;
+
+    public function __construct(Info $info, int $limit = 0)
+    {
+        $this->info = $info;
+
+        $this->setLimit($limit);
+    }
+
+    public function getInfo()
+    {
+        return $this->info->getInfo() +
+            [
+                'TradeLimit' => $this->limit,
+            ];
+    }
+
+    protected function setLimit(int $limit)
+    {
+        if (1 <= $limit && $limit <= 60) {
+            $limit = 60;
+        }
+
+        if ($limit > 900) {
+            $limit = 900;
+        }
+
+        $this->limit = $limit;
+    }
+}
