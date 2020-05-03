17c051a4ce1789b0f31066937a8fc4419b2936f3
fall1600 <fall1600@gmail.com>
2020-05-03 21:19:00 +0800

    AliPayProductInterface

---
+++ Contracts/AliPayProductInterface.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace fall1600\Package\Newebpay\Contracts;
+
+interface AliPayProductInterface
+{
+    /**
+     * 商品編號, 長度限制12位數
+     * @return int
+     */
+    public function getPid();
+
+    /**
+     * 商品名稱, 長度限制60個字元
+     * @return string
+     */
+    public function getTitle();
+
+    /**
+     * 商品說明, 長度100個字元
+     * @return string
+     */
+    public function getDescription();
+
+    /**
+     * 商品單價(新台幣), 長度限制5位數
+     * @return int
+     */
+    public function getPrice();
+
+    /**
+     * 商品數量, 長度限制2位數
+     * @return int
+     */
+    public function getQuantity();
+}
