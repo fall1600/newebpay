2bebbed194810081b5d6109bb352a8d18879b395
fall1600 <fall1600@gmail.com>
2020-05-04 20:28:37 +0800

    complete test for InfoTest

--- Tests/InfoTest.php
+++ Tests/InfoTest.php
@@ -3,15 +3,14 @@
 namespace fall1600\Package\Newebpay\Tests;
 
 use fall1600\Package\Newebpay\Constants\Language;
+use fall1600\Package\Newebpay\Contracts\OrderInterface;
+use fall1600\Package\Newebpay\Contracts\PayerInterface;
 use fall1600\Package\Newebpay\Info\Decorator\LanguageInfo;
 use fall1600\Package\Newebpay\Info\Decorator\OfflinePayInfo;
-use fall1600\Package\Newebpay\Info\Decorator\OrderInfo;
 use fall1600\Package\Newebpay\Info\Decorator\PayCompleteInfo;
 use fall1600\Package\Newebpay\Info\Decorator\PayerEmailEditableInfo;
 use fall1600\Package\Newebpay\Info\BasicInfo;
 use fall1600\Package\Newebpay\Info\Decorator\PayCancelInfo;
-use fall1600\Package\Newebpay\Tests\Mock\OrderMock;
-use fall1600\Package\Newebpay\Tests\Mock\PayerMock;
 use PHPUnit\Framework\TestCase;
 
 class InfoTest extends TestCase
@@ -22,10 +21,6 @@ class InfoTest extends TestCase
 
         $merchantId = 'merchant.id.123';
 
-        $hashKey = 'hash.key.234';
-
-        $hashIv = 'hash.iv.345';
-
         $email = 'test@gg.cc';
 
         $returnUrl= 'return.url';
@@ -34,9 +29,31 @@ class InfoTest extends TestCase
 
         $clientBackUrl = 'client.back.url';
 
-        $order = new OrderMock();
+        $order = $this->getMockBuilder(OrderInterface::class)
+            ->getMock();
 
-        $payer = new PayerMock();
+        $order->expects($this->once())
+            ->method('getMerchantOrderNo')
+            ->willReturn((string) time());
+
+        $order->expects($this->once())
+            ->method('getItemDesc')
+            ->willReturn('This is an apple');
+
+        $order->expects($this->once())
+            ->method('getAmt')
+            ->willReturn(100);
+
+        $payer = $this->getMockBuilder(PayerInterface::class)
+            ->getMock();
+
+        $payer->expects($this->once())
+            ->method('getEmail')
+            ->willReturn('foobar@gg.cc');
+
+        $payer->expects($this->once())
+            ->method('getLoginType')
+            ->willReturn(false);
 
         $ttl = 3;
 
@@ -63,8 +80,24 @@ class InfoTest extends TestCase
         ;
 
         //act
-        var_dump($info->getInfo());
+        $result = $info->getInfo();
 
         //assert
+        $this->assertArrayHasKey('MerchantID', $result);
+        $this->assertArrayHasKey('RespondType', $result);
+        $this->assertArrayHasKey('TimeStamp', $result);
+        $this->assertArrayHasKey('Version', $result);
+        $this->assertArrayHasKey('NotifyURL', $result);
+        $this->assertArrayHasKey('Amt', $result);
+        $this->assertArrayHasKey('ItemDesc', $result);
+        $this->assertArrayHasKey('MerchantOrderNo', $result);
+        $this->assertArrayHasKey('Email', $result);
+        $this->assertArrayHasKey('LoginType', $result);
+        $this->assertArrayHasKey('EmailModify', $result);
+        $this->assertArrayHasKey('LangType', $result);
+        $this->assertArrayHasKey('ClientBackURL', $result);
+        $this->assertArrayHasKey('ExpireDate', $result);
+        $this->assertArrayHasKey('CustomerURL', $result);
+        $this->assertArrayHasKey('ReturnURL', $result);
     }
 }
